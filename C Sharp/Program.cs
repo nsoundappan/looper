9c6582ca97646ba215ebd1b1205914eeebf77f10
nsoundappan <37821577+nsoundappan@users.noreply.github.com>
2018-11-26 15:56:59 -0600

    Initial Push

---
+++ C Sharp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            classB cb = new classB();
+          //  GC.Collect();
+            Console.Read();
+        }
+    }
+
+    public class classA
+    {
+        public classA()
+        {
+            Console.WriteLine("Base Class");
+        }
+
+        ~classA()
+            {
+            Console.WriteLine("Destruct Base");
+        }
+    }
+
+    public class classB: classA
+    {
+        public classB()
+        {
+            Console.WriteLine("Derived Class");
+        }
+
+        ~classB()
+        {
+            Console.WriteLine("Destruct Derived");
+        }
+    }
+}
